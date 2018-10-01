ec2726bd6154eadeb010128201711d386cea7115
Pascal Marleau-Proulx <pmarl012@uottawa.ca>
2018-10-01 11:19:11 -0400

    lab 3

---
+++ GenericCode/src/tester.java
@@ -0,0 +1,13 @@
+//This class is used to test the functioning of all the generic classes in Generic SRC
+public class tester {
+	
+	
+	//Used to create objects of all types
+public static void main(String[]args) {
+	CircularArrayQueue<Integer> circular;
+	circular = new CircularArrayQueue<Integer>(20);
+	System.out.print("Test begins");
+	
+	
+}
+}
